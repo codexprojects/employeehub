8d929c5f6b6fabbbec653fe44bd51ce58eee2f6c
İlke Yücel <yucel.ilke@gmail.com>
2024-07-12 12:51:39 +0300

    Initial commit

---
+++ EmployeeHub/EmployeeHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EmployeeHub
+//
+//  Created by Ilke Yucel on 12.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EmployeeHub/EmployeeHub/EmployeeHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmployeeHubApp.swift
+//  EmployeeHub
+//
+//  Created by Ilke Yucel on 12.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct EmployeeHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
